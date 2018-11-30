3c8f70112cf8867d2aed2300ea1bd67377d7e019
sharath-cliqz <sharath@cliqz.com>
2018-11-30 12:03:30 +0100

    Update parseReports.sh

--- scripts/azure-scripts/parseReports.sh
+++ scripts/azure-scripts/parseReports.sh
@@ -3,21 +3,24 @@ status=$(python -c '''
 import os
 errorsCount = 0
 totalCount = 0
-for report in sorted(os.listdir("test-reports")):
-	with open("test-reports/"+report) as fp:
-		for i, line in enumerate(fp):
-			if i==1:
-				for valueset in line.split(" "):
-					if "errors" in valueset or "failures" in valueset: 
-						errorsCount += int(valueset.split("\"")[1])
-					elif "tests" in valueset:
-						totalCount += int(valueset.split("\"")[1])
+try:
+	for report in sorted(os.listdir("test-reports")):
+		with open("test-reports/"+report) as fp:
+			for i, line in enumerate(fp):
+				if i==1:
+					for valueset in line.split(" "):
+						if "errors=" in valueset or "failures" in valueset: 
+							errorsCount += int(valueset.split("\"")[1])
+						elif "tests=" in valueset:
+							totalCount += int(valueset.split("\"")[1])
+except:
+	print "Seems like Tests didn't run !"
 if errorsCount == 0 and totalCount > 2:
-	print errorCount
+	print errorsCount
 elif totalCount == 0:
 	print -1
 else:
-	print errorCount
+	print errorsCount
 ''')
 if [ $status != "0" ]; then
 	echo "ERROR: Some Tests Failed !"

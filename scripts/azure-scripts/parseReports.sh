60bb73fd9b341c0b3596eddeef3d8784fa2fac2e
sharath-cliqz <sharath@cliqz.com>
2018-12-03 15:56:55 +0100

    Update parseReports.sh

--- scripts/azure-scripts/parseReports.sh
+++ scripts/azure-scripts/parseReports.sh
@@ -1,34 +1,34 @@
 echo "*** Azure Script - Parse Report XML ***"
-status=$(python -c '''
+status="$(python -c '''
 import os
 errorsCount = 0
 totalCount = 0
 try:
-	for report in sorted(os.listdir("test-reports")):
-		with open("test-reports/"+report) as fp:
-			for i, line in enumerate(fp):
-				if i==1:
-					for valueset in line.split(" "):
-						if "errors=" in valueset or "failures" in valueset: 
-							errorsCount += int(valueset.split("\"")[1])
-						elif "tests=" in valueset:
-							totalCount += int(valueset.split("\"")[1])
+    for report in sorted(os.listdir("test-reports")):
+        with open("test-reports/"+report) as fp:
+            for i, line in enumerate(fp):
+                if i==1:
+                    for valueset in line.split(" "):
+                        if "errors=" in valueset or "failures" in valueset: 
+                            errorsCount += int(valueset.split("\"")[1])
+                        elif "tests=" in valueset:
+                            totalCount += int(valueset.split("\"")[1])
 except:
-	print "Seems like Tests didn't run !"
+    print "Seems like Tests didn't run !"
 if errorsCount == 0 and totalCount > 2:
-	print errorsCount
+    print errorsCount
 elif totalCount == 0:
-	print -1
+    print -1
 else:
-	print errorsCount
-''')
-if [ $status != "0" ]; then
-	echo "ERROR: Some Tests Failed !"
-	exit 1
-elif [ $status == "-1" ]; then
-	echo "ERROR: Tests were not run !"
-	exit 1
+    print errorsCount
+''')"
+if [ "${status}" != "0" ]; then
+    echo "ERROR: Some Tests Failed !"
+    exit 1
+elif [ "${status}" == "-1" ]; then
+    echo "ERROR: Tests were not run !"
+    exit 1
 else
-	echo "All Tests Passed !"
+    echo "All Tests Passed !"
 fi
 echo "*** DONE ***"

c2666fc61840d7f60b6e25de7e73b75f9574fa1b
Sharath G Pai <sharath@cliqz.com>
2019-03-13 14:12:13 +0100

    Changed configs and add final results to a txt file

--- src/parse.py
+++ src/parse.py
@@ -9,8 +9,11 @@ with open("Cliqz.txt", "r") as fp1:
 
 result = {key: (value1, value2) for key, value1, value2 in zip(safari.keys(), safari.values(), cliqz.values())}
 
-for webpage in result:
-    list = result[webpage]
-    print("{}\n{} - {}\n{} - {}\n\n".format(webpage,
-                                            list[0].keys()[0], list[0].values()[0],
-                                            list[1].keys()[0], list[1].values()[0]))
+with open("performance.txt", "w") as fp:
+    for webpage in result:
+        list = result[webpage]
+        jointResult = "{}\n{} - {}\n{} - {}\n\n".format(webpage,
+                                                list[0].keys()[0], list[0].values()[0],
+                                                list[1].keys()[0], list[1].values()[0])
+        print(jointResult)
+        fp.write(jointResult)
